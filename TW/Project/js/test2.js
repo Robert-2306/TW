73aff1e9f156bbd8517d04b31cc9fc82287a93be
Robert-2306 <102369734+Robert-2306@users.noreply.github.com>
2022-05-26 14:57:26 +0300

    Add files via upload

--- TW/Project/js/test2.js
+++ TW/Project/js/test2.js
@@ -1,4 +1,32 @@
-function generateRandomNumber(){
-    document.getElementById("random").innerHTML = Math.floor(Math.random() * 100000000);
+function onloadFunction(){
+    document.getElementById("inputBox").style.visibility = "hidden";
+    document.getElementById("ready").style.visibility = "hidden";
+    document.getElementById("submit").style.visibility = "hidden";
+    document.getElementById("again").style.visibility = "hidden";
+}
 
+var random = Math.floor(Math.random() * 10000);
+
+function generateRandomNumber(){
+    document.getElementById("random").innerHTML = random;
+    document.getElementById("randomGen").style.visibility = "hidden";
+    document.getElementById("ready").style.visibility = "visible";
+
+}
+
+function myFunction() {
+    document.getElementById("random").style.visibility = "hidden";
+    document.getElementById("ready").style.visibility = "hidden";
+    document.getElementById("inputBox").style.visibility = "visible";
+    document.getElementById("submit").style.visibility = "visible";
+}
+
+function submitInput(){
+    let x = document.getElementById("num1").value;
+    if(random==x){
+        document.getElementById("demo").innerHTML = "Well done!";
+    }else{
+        document.getElementById("demo").innerHTML = "That was close. Keep trying!";
+    }
+    document.getElementById("again").style.visibility = "visible";
 }
